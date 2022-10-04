3556f37eb6bd58d5a4c7b444de52e6e1c68d4ae1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 23:17:14 +0000

    Adding numeric_owner as keyword arguement

--- cifar10_data.py
+++ cifar10_data.py
@@ -35,7 +35,7 @@ def load_cifar10_data():
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path="data/")
--- flower_photos_data.py
+++ flower_photos_data.py
@@ -32,7 +32,7 @@ def load_flower_datasets(limits=200):
                    if not is_within_directory(path, member_path):
                        raise Exception("Attempted Path Traversal in Tar File")
            
-               tar.extractall(path, members, numeric_owner) 
+               tar.extractall(path, members, numeric_owner=numeric_owner) 
                
            
            safe_extract(tar, path="data/")
